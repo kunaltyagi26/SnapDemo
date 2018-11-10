a6bf0b4cb3e86acd0ed4fb9051336b4109566b0a
kunaltyagi26 <kunaltyagi32@gmail.com>
2018-11-10 12:40:41 +0530

    Initial Commit

---
+++ PagingScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PagingScroll
+//
+//  Created by Kunal Tyagi on 10/11/18.
+//  Copyright © 2018 Kunal Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
